d7fd59211da6de2c393bf65afb8beee2bfabf407
melnicenkovadik <melnicenkovadik@gmail.com>
2025-11-02 14:28:39 +0100

    feat: add knigovan.com to allowed image domains
    - Add knigovan.com to remotePatterns in next.config.ts
    - Update CSP img-src to include knigovan.com in middleware.ts

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,11 @@ const nextConfig: NextConfig = {
         hostname: "dumka.top",
         pathname: "**",
       },
+      {
+        protocol: "https",
+        hostname: "knigovan.com",
+        pathname: "**",
+      },
       // Add other image domains if needed
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
